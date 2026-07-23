7de77ed5ad42e7976b719f159641c56e38ab041b
okupaylas <globalcmpny2020@gmail.com>
2026-07-23 22:21:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const express = require("express");
 
-const http = require("http");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  res.end("OkuPaylas Backend Çalışıyor 🚀");
+app.get("/", (req, res) => {
+  res.send("OkuPaylaş API çalışıyor!");
 });
 
-server.listen(3000, () => {
-  console.log("Sunucu 3000 portunda çalışıyor.");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Sunucu ${PORT} portunda çalışıyor.`);
 });
