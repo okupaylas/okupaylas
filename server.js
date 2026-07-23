2881306bdaed8a16d6874373f866d7e238038ddb
okupaylas <globalcmpny2020@gmail.com>
2026-07-23 22:18:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  res.end("OkuPaylas Backend Çalışıyor 🚀");
+});
+
+server.listen(3000, () => {
+  console.log("Sunucu 3000 portunda çalışıyor.");
+});
